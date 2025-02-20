e8133fc47de496dd2bf03996e759b21f994c2902
Nastya <anastasiia.y.sitnikova@gmail.com>
2025-02-20 16:56:32 +0100

    add solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,22 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
+    const res = arrayReverse(['hello', 'world']);
 
+    expect(Array.isArray(res)).toBeTruthy();
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it(`should not change the length of the strings
+    from the original array`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it('should reverse a single word in the array', () => {
+    expect(arrayReverse(['Hello'])).toEqual(['olleH']);
+  });
 });
